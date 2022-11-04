55f32c9f3f05482ae56166362e8e527929f8fd72
David Marino <davidjmarino8@gmail.com>
2022-11-04 17:20:24 -0400

    Add plan

---
+++ plan.rb
@@ -0,0 +1,36 @@
+# My plan moving forward:
+
+# Turing has proven itself a worthy challenge, and no worthy challenge is met without a plan.
+# Here is a list of declarations stating how I will execute my plan.
+
+# I will set alarms for 30 minutes prior to each check in to give ample time to reply.
+
+# I will ensure my calendar stays up to date.
+
+# I will follow my calendar daily.
+
+# I will prioritize quality time with my girlfriend during breaks.
+
+# I will ensure those breaks also include time to reflect.
+
+# I will buy a desk for the bedroom to allow my girlfriend the general living space while I work.
+
+# I will work out 5 times per week: Each weekend day, and 3 days of the week. Running at night clears my head.
+
+# I will schedule my medical appointments early so as to not interfere with class.
+
+# I will grocery shop on saturdays and meal prep on sundays.
+
+# I will socialize with memebers of my class.
+
+# I will study and complete assignments with my classmates.
+
+# I will take breaks.
+
+# I will continue to encourage myself that I CAN do this.
+
+# I will remember what I have overcame.
+
+# I will take my ADHD medication daily, before school but not so late that it intereferes with rest.
+
+# I will succeed.
